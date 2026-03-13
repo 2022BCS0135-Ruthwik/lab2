70cfbf3c75dfb399196e6934a34bb02172f28cb9
2022BCS0135-Ruthwik <ruthwik22bcs135@iiitkottayam.ac.in>
2026-03-13 23:57:00 +0530

    Experiment 3: RandomForest 50trees depth10 testsplit=0.3

--- train.py
+++ train.py
@@ -17,7 +17,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.3, random_state=42
 )
 
-# Model
+#Model
 model = RandomForestRegressor(n_estimators=50, max_depth=10, random_state=42)
 model.fit(X_train, y_train)
 
