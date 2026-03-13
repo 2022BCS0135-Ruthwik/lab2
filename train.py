a851b23f5c22a6ca82db076c0ad05550e84cfa4a
2022BCS0135-Ruthwik <ruthwik22bcs135@iiitkottayam.ac.in>
2026-03-14 00:16:12 +0530

    Experiment 4: RandomForest 100trees depth15 feature-subset

--- train.py
+++ train.py
@@ -10,15 +10,23 @@ from sklearn.metrics import mean_squared_error, r2_score
 # Load dataset
 data = pd.read_csv("dataset/winequality-red.csv", sep=";")
 
-X = data.drop("quality", axis=1)
+selected_features = [
+    'alcohol',
+    'sulphates',
+    'citric acid',
+    'volatile acidity',
+    'density'
+]
+
+X = data[selected_features]
 y = data["quality"]
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.3, random_state=42
+    X, y, test_size=0.2, random_state=42
 )
 
 #Model
-model = RandomForestRegressor(n_estimators=50, max_depth=10, random_state=42)
+model = RandomForestRegressor(n_estimators=100, max_depth=15, random_state=42)
 model.fit(X_train, y_train)
 
 # Predictions
