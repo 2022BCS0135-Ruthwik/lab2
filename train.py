2f8585e13addea0bc0947c6d95ddd01b59dd0ecb
2022BCS0135-Ruthwik <ruthwik22bcs135@iiitkottayam.ac.in>
2026-03-13 23:51:01 +0530

    Experiment 2: Ridge alpha=1.0, preprocessing-standardscaler

--- train.py
+++ train.py
@@ -4,7 +4,8 @@ import joblib
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
+from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error, r2_score
 
 # Load dataset
@@ -16,9 +17,12 @@ y = data["quality"]
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
+scaler = StandardScaler()
 
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
 # Model
-model = LinearRegression()
+model = Ridge(alpha=1.0)
 model.fit(X_train, y_train)
 
 # Predictions
